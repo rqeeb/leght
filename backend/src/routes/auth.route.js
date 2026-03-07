fb24744c243b2c96569230ae204a2f7ceaf067f5
RaqeebDev <raqeebauto@gmail.com>
2026-03-07 08:43:13 +0530

    check route

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,7 +1,11 @@
 import express from "express";
-import { signup, login, logout, updateProfile } from "../controllers/auth.controller.js";
+import {
+  signup,
+  login,
+  logout,
+  updateProfile,
+} from "../controllers/auth.controller.js";
 import { projectRoute } from "../middleware/auth.middleware.js";
-
 
 const router = express.Router();
 
@@ -12,5 +16,9 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 router.put("/updateProfile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, (req, res) => {
+  res.status(200).json(req.user);
+});
 
 export default router;
