e90608ec5b1e45572c7ea57359e43d9b40f9bcd6
RaqeebDev <raqeebauto@gmail.com>
2026-03-14 16:57:13 +0530

    base background and grid

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
+import { Route, Routes } from "react-router";
+import ChatPage from "./pages/ChatPage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+
 function App() {
   return (
-    <div>
-     <button className = "btn btn-primary"> Button</button>
+    <div className="min-h-screen bg-[#D4CAB7] relative flex items-center justify-center p-4 overflow-hidden">
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f14_1px,transparent_1px)] bg-[size:14px_24px]" />
+      <div className="absolute top-0 -left-4 size-96 bg-[rgb(218,90,56)] opacity-20 blur-[100px]" />
+      <div className="absolute bottom-0 -right-4 size-96 bg-[rgb(218,90,56)] opacity-20 blur-[100px]" />
+
+      <Routes>
+        <Route path="/" element={<ChatPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
     </div>
   );
-
 }
 
 export default App;
