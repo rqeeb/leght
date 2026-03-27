2c62456bdd2b0d4d20ba26ae2551f9620fe00892
RaqeebDev <raqeebauto@gmail.com>
2026-03-27 16:22:55 +0530

    Avatar png + profile header imports + profile pic upload functionality

--- frontend/src/components/ProfileHeader.jsx
+++ frontend/src/components/ProfileHeader.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+
+import { useState, useRef } from "react";
+import { LogOutIcon, VolumeOffIcon, Volume2Icon } from "lucide-react";
+import { useAuthStore } from "../store/useAuthStore";
+import { useChatStore } from "../store/useChatStore";
 
 function ProfileHeader() {
+  //States
+  const { logout, authUser, updateProfile } = useAuthStore();
+  const { isSoundEnabled, toggleSound } = useChatStore();
+  const { selectedImg, setSelectedImg } = useState(null);
+
+  const fileInputRef = useRef(null);
+
+  const handleImageUpload = (e) => {};
+
   return (
-    <div>
-      
+    <div className="p-6 border-b border-slate-700/50  ">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          {/* Avatar */}
+          <div className="avatar online">
+            <button>
+              
+            </button>
+            <input
+              type="file"
+              accept="image/*"
+              ref={fileInputRef}
+              className="hidden"
+            />
+
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default ProfileHeader;
