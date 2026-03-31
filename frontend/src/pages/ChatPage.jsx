c5544bf5cf6122ab168ff108a766bc37b1d1b40c
RaqeebDev <raqeebauto@gmail.com>
2026-03-31 20:17:32 +0530

    Major ChatComponents: contacts and contactlist - sidebar

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -9,7 +9,7 @@ import ChatContainer from "../components/ChatContainer";
 import NoConversationPlaceholder from "../components/NoConversationPlaceholder";
 
 const ChatPage = () => {
-  const { activeTab, selectedUser } = useChatStore;
+  const { activeTab, selectedUser } = useChatStore();
 
   return (
     <div className="relative w-full max-w-6xl h-[800px]">
@@ -18,13 +18,14 @@ const ChatPage = () => {
         <div className="w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col ">
           <ProfileHeader />
           <ActiveTabSwitch />
-          <div className="flex-1 overflow-y-auto p-4 space-y-2 ">
+
+          <div className="flex-1 overflow-y-auto p-4 space-y-2">
             {activeTab === "chats" ? <ChatsList /> : <ContactList />}
           </div>
         </div>
 
         {/* Right SIDE */}
-        <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm  ">
+        <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm">
           {selectedUser ? <ChatContainer /> : <NoConversationPlaceholder />}
         </div>
       </BorderAnimatedContainer>
