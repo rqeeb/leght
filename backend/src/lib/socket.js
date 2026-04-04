c33998ef78f63feb0ce229f592a3002c22e5278a
RaqeebDev <raqeebauto@gmail.com>
2026-04-04 16:19:06 +0530

    scoket donew

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -9,9 +9,36 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    orgin: [ENV.CLIENT_URL],
+    origin: [ENV.CLIENT_URL],
     credentials: true,
   },
 });
 
 io.use(socketAuthMiddleware);
+
+
+export function getReceiverSocketId(userId) {
+  return userSocketMap[userId];
+}
+
+
+const userSocketMap = {}; 
+
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.user.fullName);
+
+  const userId = socket.userId;
+  userSocketMap[userId] = socket.id;
+
+  
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+ 
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.user.fullName);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
+
+export { io, app, server };
