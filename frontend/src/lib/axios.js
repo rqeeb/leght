0bc7da69e8961249883b0d3efef8155753ba2a83
RaqeebDev <raqeebauto@gmail.com>
2026-04-06 13:11:07 +0530

    url change

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:2022/api"
-      : "https://zonnect.vercel.app/api",
+      : "https://zonnect.onrender.com/",
   withCredentials: true,
 });
